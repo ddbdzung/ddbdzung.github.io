4cd9b56fb1d038d548049f70aae21d5e14827bc2
DzungDang <ddbdzung.wa@gmail.com>
2021-10-31 21:01:29 +0700

    finish day_3

---
+++ Day_3/script.js
@@ -0,0 +1,81 @@
+function mousedown(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "grey";
+    obj.style.color = "black";
+}
+function mouseup(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.color = "black";
+    obj.style.backgroundColor = "lightgrey";
+}
+function mouseover(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "lightgrey";
+    obj.style.color = "black";
+}
+function mouseout(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "white";
+    obj.style.color = "black";
+}
+
+/*
+------------------------------------------------------
+*/
+
+function mousedown1(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "grey";
+    obj.style.color = "white";
+}
+function mouseup1(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "lightgrey";
+    obj.style.color = "black";
+}
+function mouseover1(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "lightgrey";
+    obj.style.color = "black";
+}
+function mouseout1(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "black";
+    obj.style.color = "white";
+}
+
+/*
+------------------------------------------------------
+*/
+
+function mousedown2(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "grey";
+    obj.style.color = "black";
+}
+function mouseup2(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.color = "black";
+    obj.style.backgroundColor = "lightgrey";
+}
+function mouseover2(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "lightgrey";
+    obj.style.color = "black";
+}
+function mouseout2(obj) {
+    obj.style.border = "1.25px solid black";
+    obj.style.backgroundColor = "white";
+    obj.style.color = "black";
+}
+
+/*
+------------------------------------------------------
+*/
+
+function mousedown0(obj) {
+    obj.style.border = "3px solid red";
+}
+function mouseup0(obj) {
+    obj.style.border = "1px solid black";
+}
